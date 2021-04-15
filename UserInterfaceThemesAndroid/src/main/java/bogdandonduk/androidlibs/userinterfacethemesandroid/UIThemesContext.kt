589b37f1057795e9abcac0ecccf4dd6674134520
bogdandonduk <bogdandonduk@gmail.com>
2021-04-15 19:28:33 +0700

    Commit 3

--- UserInterfaceThemesAndroid/src/main/java/bogdandonduk/androidlibs/userinterfacethemesandroid/UIThemesContext.kt
+++ UserInterfaceThemesAndroid/src/main/java/bogdandonduk/androidlibs/userinterfacethemesandroid/UIThemesContext.kt
@@ -2,6 +2,9 @@ package bogdandonduk.androidlibs.userinterfacethemesandroid
 
 import android.content.Context
 import android.content.res.Configuration
+import android.content.res.Resources
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 
 object UIThemesContext {
     private const val LIBRARY_PREFIX = "prefs_bogdandonduk.androidlibs.userinterfacethemesandroid_"
@@ -11,7 +14,27 @@ object UIThemesContext {
         context.getSharedPreferences(LIBRARY_PREFIX + context.packageName, Context.MODE_PRIVATE)
             .getBoolean(IS_DARK_THEME_ENABLED, context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES)
 
-    fun setDarkTheme(context: Context, enabled: Boolean = true) {
+    fun setDarkTheme(context: Context, enabled: Boolean) {
         context.getSharedPreferences(LIBRARY_PREFIX + context.packageName, Context.MODE_PRIVATE).edit().putBoolean(IS_DARK_THEME_ENABLED, enabled).apply()
     }
+
+    fun initializeTextTheme(context: Context, vararg texts: TextView, colorResId: Int) {
+        val darkTheme = isDarkThemeEnabled(context)
+
+        texts.forEach {
+            it.setTextColor(ResourcesCompat.getColor(getConfiguredResources(context, darkTheme), colorResId, null))
+        }
+    }
+
+    fun initializeTextTheme(context: Context, vararg texts: TextView, lightThemeColorResId: Int, darkThemeColorResId: Int) {
+        val darkTheme = isDarkThemeEnabled(context)
+
+        texts.forEach {
+            it.setTextColor(ResourcesCompat.getColor(context.resources, if(darkTheme) darkThemeColorResId else lightThemeColorResId, null))
+        }
+    }
+
+    private fun getConfiguredResources(context: Context, darkTheme: Boolean) : Resources =
+            context.createConfigurationContext(Configuration(context.resources.configuration).apply { uiMode = if(darkTheme) Configuration.UI_MODE_NIGHT_YES else Configuration.UI_MODE_NIGHT_NO }).resources
+
 }
